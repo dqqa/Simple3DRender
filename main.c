29601c2fce40a8d550f5978f01cc7a3d93b8fa45
Nikita <zhni23u172@student.bmstu.ru>
2025-02-06 22:41:10 +0300

    Implement rotation

--- main.c
+++ main.c
@@ -21,12 +21,27 @@
 #define BACKGROUND_COLOR (Color{ 0x18, 0x18, 0x18, 0xff })
 #endif
 
-#define ROTATION_SPEED 0.01f
+#define FOV 10.0f // Not shure about what exactly this is
+
+#define SCALE 100.0f
+
+#define LINE_WIDTH 1
+#define ROTATION_SPEED (DEG2RAD * 1.f)
 
 #define ARR_LEN(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 // Why not? :)
 #include "cube.h"
+
+Vector2 projection(const Vector3 *point3d, float fov)
+{
+    Vector2 res;
+
+    res.x = (fov * point3d->x) / (fov + point3d->z);
+    res.y = (fov * point3d->y) / (fov + point3d->z);
+
+    return res;
+}
 
 int main(int argc, char *argv[])
 {
@@ -56,20 +71,42 @@ int main(int argc, char *argv[])
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     SetTargetFPS(TARGET_FPS);
 
+    float rotation = 0.0f;
+
     while (!WindowShouldClose())
     {
         float dt = GetFrameTime();
         (void)dt;
         BeginDrawing();
         ClearBackground(BACKGROUND_COLOR);
+        // Vector2 mouse_pos = GetMousePosition();
 
         // Render
+
+        for (size_t vert = 0; vert < cube_model.vert_cnt; vert++)
+        {
+            Vertex *v = &cube_model.vertices[vert];
+
+            Vector3 new_start3d = Vector3RotateByAxisAngle(cube_model.points[v->start], (Vector3){ 0, 0, 1 }, rotation);
+            Vector3 new_end3d = Vector3RotateByAxisAngle(cube_model.points[v->end], (Vector3){ 0, 0, 1 }, rotation);
+
+            Vector2 start = projection(&new_start3d, FOV);
+            Vector2 end = projection(&new_end3d, FOV);
+            start = Vector2Add(Vector2Scale(start, SCALE), (Vector2){ WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0 });
+            end = Vector2Add(Vector2Scale(end, SCALE), (Vector2){ WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0 });
+
+            DrawLineV(start, end, WHITE);
+        }
+
 #ifdef DEBUG
         char buf[256];
         snprintf(buf, sizeof(buf), "FPS: %7.2f", 1.0f / dt);
         DrawText(buf, 10, 10, 20, WHITE);
 #endif
-
+        if (rotation + ROTATION_SPEED >= 360)
+            rotation = rotation + ROTATION_SPEED - 360;
+        else
+            rotation += ROTATION_SPEED;
         EndDrawing();
     }
 
